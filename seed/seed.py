c56f0b4cef2c7adad02e15a03705040cc5ee5c8f
MinZung <90918826+minza55113151@users.noreply.github.com>
2023-02-07 14:57:13 +0700

    last

--- seed/seed.py
+++ seed/seed.py
@@ -1,50 +1,52 @@
+import os
+import urllib
+
+from dotenv import load_dotenv
 from pymongo import MongoClient
 
-USERNAME = "exceed11"
-PASSWORD = "sRS47gYL"
-DB_NAME = "exceed11"
-MONGO_DB_PORT = 8443
-MONGO_DB_URL = f"mongodb://{USERNAME}:{PASSWORD}@mongo.exceed19.online:{MONGO_DB_PORT}"
+load_dotenv('.env')
 
-LOCKER_COLLECTION_NAME = "locker"
+exceeduser = os.getenv('exceeduser')
+password = os.getenv('password')
+
+client = MongoClient(f"mongodb://{exceeduser}:{password}@mongo.exceed19.online:8443/?authMechanism=DEFAULT")
+db = client['exceed11']
 
 SEED_DATA = [
     {
-        "locker_id": 0,
+        "id": 0,
         "available": True,
         "reservation_id": None
     },
     {
-        "locker_id": 1,
+        "id": 1,
         "available": True,
         "reservation_id": None
     },
     {
-        "locker_id": 2,
+        "id": 2,
         "available": True,
         "reservation_id": None
     },
     {
-        "locker_id": 3,
+        "id": 3,
         "available": True,
         "reservation_id": None
     },
     {
-        "locker_id": 4,
+        "id": 4,
         "available": True,
         "reservation_id": None
     },
     {
-        "locker_id": 5,
+        "id": 5,
         "available": True,
         "reservation_id": None
     },
 ]
 
 def main():
-    client = MongoClient(MONGO_DB_URL)
-    db = client[DB_NAME]
-    collection = db[LOCKER_COLLECTION_NAME]
+    collection = db["locker"]
     collection.delete_many({})
     collection.insert_many(SEED_DATA)
     print("Seed data inserted successfully.")
